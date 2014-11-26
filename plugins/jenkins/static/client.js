1e50523ad1e1e085930ba878713402538fb4d071
Eugene Venter <eugene@catalyst.net.nz>
2014-11-27 09:08:18 +1300

    plugins/jenkins: rewrite
    - simplify
    - now more robust and supports any amount of jobs too :)

--- plugins/jenkins/static/client.js
+++ plugins/jenkins/static/client.js
@@ -11,19 +11,12 @@ plugins.jenkins = {
 
     receiveData: function(data) {
 
-        // Check for a single message (is not array?)
-        if (!$.isArray(data)) {
-            data = [data];
-        }
-
         var container = $('div#jenkins ol');
+        container.html('');
 
-        // Save old queue count for comparison later
-        var oldqueue = this.queue;
-        this.queue = 0;
-
-        for (c in data) {
-            var content = data[c];
+        // update builds
+        for (c in data[0].builds) {
+            var content = data[0].builds[c];
 
             if (content == 1) {
                 // Ignore (this is a placeholder)
@@ -34,15 +27,8 @@ plugins.jenkins = {
             var status = content.status;
             var statusclass = 'status-'+status;
             var name = content.name;
-            var duration = content.duration;
-            var href = content.href;
-
-            if (status == 'RUNNING') {
-                duration = status;
-
-                // Update queue count (only running jobs have a queue property)
-                this.queue = content.queue;
-            }
+            var duration = status == 'RUNNING' ? 'RUNNING' : content.duration;
+            var href = content.url;
 
             var link = $('<a>').attr('href', href).html(name);
             var dur = $('<span class="duration">').html(duration);
@@ -51,73 +37,26 @@ plugins.jenkins = {
             node.html(link).append(dur);
             node.hide();
 
-            // Check if this is an update (as the previous build already exists)
-            var exists = $('li#'+id, container);
-            if (exists.length) {
-                if (exists.attr('class') !== statusclass || status == 'RUNNING') {
-                    console.log('replace');
-                    exists.replaceWith(node);
-                    node.show();
-                }
-                else {
-                    console.log('no change');
-                }
-
-                continue;
-            }
-
-            // Find the next highest build time and add after that
-            var buildsort = content.sort;
-            var next = null;
-            var nextid = null;
-            $('li', container).each(function() {
-                var check = $(this).data('sort');
-                if (check > buildsort) {
-                    if (next) {
-                        if (next > check) {
-                            next = check;
-                            nextid = $(this).data('buildid');
-                        }
-                    } else {
-                        next = check;
-                        nextid = $(this).data('buildid');
-                    }
-                }
-            });
-
-            if (next) {
-                // If found next highest, append after
-                $('li#build-'+nextid, container).after(node);
-            } else {
-                // Otherwise is highest, add to top
-                container.prepend(node);
-            }
+            container.append(node);
 
             node.fadeIn(1000);
         }
 
-        if ($('li', container).length > 30) {
-            $('li:last', container).slideUp().remove();
+
+        // Update queue
+        queue = data[0].queue
+        var qcontainer = $('div#jenkins h1 span.queue');
+        if (!qcontainer.length) {
+            qcontainer = $('<span class="queue"></span>');
+            $('div#jenkins h1').append(qcontainer);
         }
 
-        // If queue length has changed, update it
-        if (oldqueue != this.queue) {
-            console.log('Update queue');
-
-            // Update queue
-            var qcontainer = $('div#jenkins h1 span.queue');
-            if (!qcontainer.length) {
-                qcontainer = $('<span class="queue"></span>');
-                $('div#jenkins h1').append(qcontainer);
-            }
-
-            if (this.queue == 0) {
-                qcontainer.text('');
-            } else if (this.queue == 1) {
-                qcontainer.text('['+this.queue+' build in queue]');
-            } else {
-                qcontainer.text('['+this.queue+' builds in queue]');
-            }
+        if (queue.count == 0) {
+            qcontainer.text('');
+        } else if (queue.count == 1) {
+            qcontainer.text('[1 build in queue: '+queue.items[0]+']');
+        } else {
+            qcontainer.text('['+queue.count+' builds in queue - next: '+queue.items[0]+']');
         }
     }
 }
